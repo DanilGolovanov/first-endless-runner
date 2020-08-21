383f32846b62309c199396531c13b31ea5c908c5
DanilGolovanov <golovanov.da1@gmail.com>
2020-08-21 19:11:39 +1000

    Initial commit
    Added:
    -character movement, animations (used placeholder for now)
    -randomised platform creation

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    #region Variables
+
+    public GameObject platform;
+    public Transform generationPoint;
+    private float distanceBetween;
+
+    public float minDistanceBetween;
+    public float maxDistanceBetween;
+
+    private int platformSelector;
+    private float[] platformWidths;
+
+    public ObjectPooler[] objectPools;
+
+    private float minHeight;
+    public Transform maxHeightPoint;
+    private float maxHeight;
+    public float maxHeightChange;
+    private float heightChange;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformWidths = new float[objectPools.Length];
+        for (int i = 0; i < objectPools.Length; i++)
+        {
+            platformWidths[i] = objectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x; 
+        }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            //pick random distance between platforms
+            distanceBetween = Random.Range(minDistanceBetween, maxDistanceBetween);
+
+            //pick random platform size from the array of prefabs
+            platformSelector = Random.Range(0, objectPools.Length);
+
+            //change current position of the platform generator by random value
+            heightChange = transform.position.y + Random.Range(-maxHeightChange, maxHeightChange);
+
+            //force height of the platform to be within screen height
+            heightChange = Mathf.Clamp(heightChange, minHeight, maxHeight);
+
+            //set position for a new platform
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
+
+            //create new platform 
+            GameObject newPlatform = objectPools[platformSelector].GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+
+            //avoid overlapping platforms
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D _myRigidbody;
+    private Collider2D _myCollider;
+    private Animator _myAnimator;
+
+    private bool grounded = false;
+    private bool doubleJumpAllowed = false;
+
+    #endregion
+
+    #region Default Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRigidbody = GetComponent<Rigidbody2D>();
+        _myCollider = GetComponent<Collider2D>();
+        _myAnimator = GetComponent<Animator>();
+        jumpTimeCounter = jumpTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveForward();
+        Jump();
+        ChangeAnimations();
+    }
+
+    #endregion 
+
+    #region Custom Methods
+
+    void Jump()
+    {
+        //check if player is on the ground
+        grounded = Physics2D.IsTouchingLayers(_myCollider, whatIsGround);
+
+        //if player is on the ground allow double jump
+        if (grounded)
+        {
+            doubleJumpAllowed = true;
+            jumpTimeCounter = jumpTime;
+        }
+
+        //use "space" button to jump
+        //jump if player is on the ground
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            SingleJump();
+        }
+        //jump second time if player is in the air
+        else if (doubleJumpAllowed && Input.GetKeyDown(KeyCode.Space))
+        {
+            SingleJump();
+            doubleJumpAllowed = false;
+        }
+
+        //jump higher if player holds space
+        if (jumpTimeCounter > 0 && Input.GetKey(KeyCode.Space))
+        {
+            SingleJump();
+            jumpTimeCounter -= Time.deltaTime;
+            Debug.Log(jumpTimeCounter);
+        }
+        //reset jump time counter if player doesn't hold space anymore
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpTimeCounter = 0;
+        }
+    }
+
+    void SingleJump()
+    {
+        //move player up on y-axis
+        _myRigidbody.velocity = new Vector2(_myRigidbody.velocity.x, jumpForce);
+    }
+
+    void MoveForward()
+    {
+        //constantly move player forward
+        _myRigidbody.velocity = new Vector2(moveSpeed, _myRigidbody.velocity.y);
+    }
+
+    void ChangeAnimations()
+    {
+        _myAnimator.SetFloat("Speed", _myRigidbody.velocity.x);
+        _myAnimator.SetBool("Grounded", grounded);
+    }
+
+    #endregion
+}
